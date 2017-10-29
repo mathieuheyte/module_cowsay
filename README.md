6d562c9d90e8e5083f678c2c76da8b3ec27559cb
mathieuheyte <mathieuheyte3@gmail.com>
2017-10-29 20:58:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Installation and using the cosway module, then displaying a cow saying "hello boy"
---
+++ index.js
@@ -0,0 +1,7 @@
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "hello boy",
+	e : "oO",
+	T : "U "
+}));
